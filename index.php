0c810b94e712072e6a9f0834dd2945db7b0f55e6
lukaszmpiasecki <lukasz.piasecki@int.pl>
2019-11-25 17:39:28 +0100

    START

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<body>
+<h1>Witaj na naszej stronie</h1>
+<h2>Zaloguj się lub zarejestruj</h2>
+<form action="login.php" method="post">
+<input type="text" name="login"></input>
+</br></br>
+<input type="password" name="haslo"></input>
+</br></br>
+<input type="submit" value="Zaloguj"></input>
+</form>
+<a href="rejestracja.php"><input type="button" value="Zarejestruj się za darmo"></input></a>
+</body>
+</html>
