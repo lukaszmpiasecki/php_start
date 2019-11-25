28c1b7fbfdd873e8b429ad5bed39794a125175e7
lukaszmpiasecki <lukasz.piasecki@int.pl>
2019-11-25 18:58:38 +0100

    2

--- login.php
+++ login.php
@@ -1,4 +1,22 @@
 
 <?php
-echo "Strona w budowie";
+
+require_once "connect_sql.php";
+
+$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
+
+if($polaczenie->connect_errno!=0)
+{
+	echo "ERROR";
+}
+else{
+	echo "CONNECT TO DATABASE";
+	$polaczenie->close();
+}
+#$login = $_POST['login'];
+#$haslo = $_POST['haslo'];
+
+
+
+
 ?>
