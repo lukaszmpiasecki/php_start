692558cb75f2e5902f815f9c236c3d240df0028b
lukaszmpiasecki <lukasz.piasecki@int.pl>
2019-12-22 13:41:38 +0100

    ADD SQL CHECK

--- login.php
+++ login.php
@@ -7,12 +7,24 @@ $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
 
 if($polaczenie->connect_errno!=0)
 {
-	echo "ERROR";
+	echo "ERROR: ".$polaczenie->connect_errno;
 }
 else{
 	$login = $_POST['login'];
 	$haslo = $_POST['haslo'];
-	echo "CONNECT TO DATABASE";
+	$zapytanie = "SELECT * FROM CZYTELNICY WHERE login='$login' AND haslo='$haslo'";
+	if ($wynik = $polaczenie->query($zapytanie))
+	{
+		$ile = $wynik->num_rows;
+		if ($ile>0)
+		{
+			echo "JEST TAKI";
+		}
+		else
+		{
+			echo "NIE MA TAKIEGO NUMERU";
+		}
+	}
 	$polaczenie->close();
 }
 
