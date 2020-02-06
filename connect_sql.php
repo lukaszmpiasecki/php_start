daeaf7519a7cbe7f31997966aba359400ba58e7e
lukaszmpiasecki <lukasz.piasecki@int.pl>
2020-02-06 22:02:55 +0100

    Database again on localhost

--- connect_sql.php
+++ connect_sql.php
@@ -1,15 +1,15 @@
 <?php
-
+/*
 $host="serwer1936180.home.pl";
 $db_user = "30992658_db";
 $db_password = "K@spersky";
 $db_name = "30992658_db";
-/*
+*/
 
 $host="127.0.0.1";
 $db_user = "root";
 $db_password = "";
 $db_name = "biblioteka";
 
-*/
+
 ?>
